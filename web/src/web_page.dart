18af2ac9fb3c4ff0fe6510626557c8c5541e4e1c
damien <devnull@localhost>
2013-08-12 11:20:52 +0200

    added another workaround for bug 12143 (the save menu was not showing)

--- web/src/web_page.dart
+++ web/src/web_page.dart
@@ -52,7 +52,13 @@ class WebPage {
   }
   
   void openDocument(String filePath, String configPath) {
-    filePath = '' + filePath; // http://code.google.com/p/dart/issues/detail?id=12143
+    // workarounds for Dart bug 12143, should be removed once this is fixed
+    // see http://code.google.com/p/dart/issues/detail?id=12143
+    filePath = '' + filePath;
+    doc.saveURL = '${doc.saveURL}';
+    if (doc.saveURL == '' || doc.saveURL == 'null')
+      doc.saveURL = null;
+    
     doc.openDocument(filePath, configPath).then( (_) {
       _buildMenus();
       init();
